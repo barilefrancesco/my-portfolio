ca7d275145436263b900bdd65cafbc486598ded7
barilefrancesco <barile.ciccio98@gmail.com>
2024-10-06 18:30:59 +0200

    added work experiences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,50 +28,136 @@ export default function HomePage() {
       endDate: "Present",
       experiences: [
         {
-          description: `
-          Developed a booking engine for accommodations and experiences in Praia a Mare (CS)
-            o Designed and implemented the booking system's architecture, including user interface design with React and backend services using NodeJS.
-            o Integrated third-party APIs for real-time availability and pricing.
-            o Implemented secure payment gateways and user authentication systems to ensure data protection and seamless transactions        
-          `,
+          description: (
+            <>
+              <p>
+                Developed a booking engine for accommodations and experiences in
+                Praia a Mare (CS)
+              </p>
+              <ul>
+                <li>
+                  {`Designed and implemented the booking system's architecture,
+                  including user interface design with React and backend
+                  services using NodeJS.`}
+                </li>
+                <li>
+                  Integrated third-party APIs for real-time availability and
+                  pricing.
+                </li>
+                <li>
+                  Implemented secure payment gateways and user authentication
+                  systems to ensure data protection and seamless transactions
+                </li>
+              </ul>
+            </>
+          ),
         },
         {
-          description: `
-          Developed a SaaS solution for creating showcase websites
-            o Architected a multi-tenant system to allow different users to create and manage their own websites.
-            o Utilized NextJS for server-side rendering and React for a dynamic, responsive user interface.
-            o Integrated customizable templates and a drag-and-drop page builder to enhance user experience and flexibility.
-          `,
+          description: (
+            <>
+              <p>Developed a SaaS solution for creating showcase websites</p>
+              <ul>
+                <li>
+                  Architected a multi-tenant system to allow different users to
+                  create and manage their own websites.
+                </li>
+                <li>
+                  Utilized NextJS for server-side rendering and React for a
+                  dynamic, responsive user interface.
+                </li>
+                <li>
+                  Integrated customizable templates and a drag-and-drop page
+                  builder to enhance user experience and flexibility.
+                </li>
+              </ul>
+            </>
+          ),
         },
         {
-          description: `
-          Developed a Flutter app for home automation and smart home management
-            o Created a cross-platform mobile app using Flutter, enabling users to control smart home devices such as lights, thermostats, and security systems.
-            o Focused on a user-friendly interface with intuitive navigation and control panels.          
-          `,
+          description: (
+            <>
+              <p>
+                Developed a Flutter app for home automation and smart home
+                management
+              </p>
+              <ul>
+                <li>
+                  Created a cross-platform mobile app using Flutter, enabling
+                  users to control smart home devices such as lights,
+                  thermostats, and security systems.
+                </li>
+                <li>
+                  Focused on a user-friendly interface with intuitive navigation
+                  and control panels.
+                </li>
+              </ul>
+            </>
+          ),
         },
         {
-          description: `
-          Developed a SaaS web application for online booking management
-            o Built a full-stack solution using PHP and WordPress.
-            o Implemented features such as reservation management, calendar integration, and automated email and SMS notifications.
-          `,
+          description: (
+            <>
+              <p>
+                Developed a SaaS web application for online booking management
+              </p>
+              <ul>
+                <li>Built a full-stack solution using PHP and WordPress.</li>
+                <li>
+                  Implemented features such as reservation management, calendar
+                  integration, and automated email and SMS notifications.
+                </li>
+              </ul>
+            </>
+          ),
         },
         {
-          description: `
-          Developed an e-commerce solution using NextJS and MedusaJS
-            o Leveraged the headless architecture of MedusaJS for flexible backend management and NextJS for fast, SEO-friendly front-end rendering.
-            o Implemented a comprehensive product catalog, inventory management, and dynamic pricing models.
-            o Focused on a modular design approach, allowing for easy feature updates and scaling.          
-          `,
+          description: (
+            <>
+              <p>Developed an e-commerce solution using NextJS and MedusaJS</p>
+              <ul>
+                <li>
+                  Leveraged the headless architecture of MedusaJS for flexible
+                  backend management and NextJS for fast, SEO-friendly front-end
+                  rendering.
+                </li>
+                <li>
+                  Implemented a comprehensive product catalog, inventory
+                  management, and dynamic pricing models.
+                </li>
+                <li>
+                  Focused on a modular design approach, allowing for easy
+                  feature updates and scaling.
+                </li>
+                <li>
+                  Developed a modular design approach, allowing for easy feature
+                  updates and scaling.
+                </li>
+              </ul>
+            </>
+          ),
         },
         {
-          description: `
-          Developed an e-commerce platform with WordPress and WooCommerce
-            o Customized WooCommerce plugins and themes to cater to specific business requirements
-            o Implemented payment gateway integrations, including PayPal and Stripe, to provide multiple payment options.
-            o Enhanced SEO and site performance to improve user engagement and sales conversions.
-          `,
+          description: (
+            <>
+              <p>
+                Developed an e-commerce platform with WordPress and WooCommerce
+              </p>
+              <ul>
+                <li>
+                  Customized WooCommerce plugins and themes to cater to specific
+                  business requirements
+                </li>
+                <li>
+                  Implemented payment gateway integrations, including PayPal and
+                  Stripe, to provide multiple payment options.
+                </li>
+                <li>
+                  Enhanced SEO and site performance to improve user engagement
+                  and sales conversions.
+                </li>
+              </ul>
+            </>
+          ),
         },
       ],
     },
@@ -84,29 +170,23 @@ export default function HomePage() {
       experiences: [
         {
           description: (
-            <p>
-              Developed a pediatric anamnesis booking management software,
-              handling the software analysis, database design, and
-              front-end/back-end development.
-            </p>
-          ),
-        },
-        {
-          description: (
-            <p>
-              Created a shift management system for pharmacies in the city of
-              Bitonto, including requirement analysis, implementation, and
-              deployment.
-            </p>
-          ),
-        },
-        {
-          description: (
-            <p>
-              Built and customized several websites using WordPress, focusing on
-              both theme and plugin development to enhance functionality and
-              user experience.
-            </p>
+            <ul>
+              <li>
+                Developed a pediatric anamnesis booking management software,
+                handling the software analysis, database design, and
+                front-end/back-end development.
+              </li>
+              <li>
+                Created a shift management system for pharmacies in the city of
+                Bitonto, including requirement analysis, implementation, and
+                deployment.
+              </li>
+              <li>
+                Built and customized several websites using WordPress, focusing
+                on both theme and plugin development to enhance functionality
+                and user experience.
+              </li>
+            </ul>
           ),
         },
       ],
--- src/components/portfolio/work-experience.tsx
+++ src/components/portfolio/work-experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { type WorkExperience } from "~/lib/types";
 
 export function WorkExperienceList({
@@ -32,13 +33,36 @@ function WorkExperience({
           <p className="text-xl font-semibold tracking-[-0.01em]">
             {workExperience.company}
           </p>
-          <p className="text-green-700">{workExperience.position}</p>
+          <p className="text-green-600">{workExperience.position}</p>
         </div>
         <div>
           <p>
             {workExperience.startDate} - {workExperience.endDate}
           </p>
         </div>
+      </div>
+      <div>
+        {workExperience.experiences.map((experience, index) => (
+          <div
+            key={"experience" + index}
+            className="flex flex-col gap-2 pl-[1rem]"
+          >
+            <div className="text-md">{experience.description}</div>
+            {experience.image && (
+              <Image
+                src={experience.image}
+                alt={
+                  typeof experience.description === "string"
+                    ? experience.description
+                    : "experience: " + index
+                }
+                width={200}
+                height={200}
+                className="rounded-full border-[0.5px] border-gray-800 shadow-xl"
+              />
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
