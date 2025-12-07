2e7ab4942ca097fece71f896a1e9dd832c44438f
barilefrancesco <francescobarile@proton.me>
2025-12-07 17:17:37 +0100

    Prisma to v7

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,17 +1,18 @@
 import { env } from "~/env";
-import { PrismaClient } from "@prisma/client";
-
+import { PrismaClient } from '../../../prisma/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+const adapter = new PrismaPg({ 
+  connectionString: process.env.POSTGRES_URL 
+});
+
 export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    log: env.NODE_ENV === "development" ? ["error"] : ["error"],
-  });
+  global.prisma ?? new PrismaClient({ adapter });
 
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
